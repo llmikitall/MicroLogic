746e6ed1a9fb2a166142ced76a0b9e49ba02c3b1
MikAndTon <MikAndTon@yandex.ru>
2025-11-06 11:18:08 +0300

    v06.11.25

--- src/main/java/ru/project/Application.java
+++ src/main/java/ru/project/Application.java
@@ -1,4 +1,54 @@
 package ru.project;
 
-public class Application {
+import com.jme3.app.FlyCamAppState;
+import com.jme3.app.SimpleApplication;
+import com.jme3.app.StatsAppState;
+import com.jme3.light.DirectionalLight;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Spatial;
+import com.jme3.system.AppSettings;
+
+public class Application extends SimpleApplication {
+
+    @Override
+    public void simpleInitApp(){
+
+        Spatial model = assetManager.loadModel("/Models/logic.obj");
+        Material material = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        material.setColor("Color", ColorRGBA.White);
+        model.setMaterial(material);
+        model.setLocalScale(0.1f);
+        model.rotate(0, 0, FastMath.DEG_TO_RAD * 90);
+        rootNode.attachChild(model);
+
+        /*
+        DirectionalLight sun = new DirectionalLight();
+        sun.setDirection(new Vector3f(-1, -2, -3).normalizeLocal());
+        rootNode.addLight(sun);
+
+
+         */
+    }
+
+    public Application(){
+        super(new FlyCamAppState(), new StatsAppState());
+    }
+
+    public static void main(String[] args){
+        Application application = new Application();
+
+        AppSettings settings = new AppSettings(true);
+        settings.setWidth(1280);
+        settings.setHeight(720);
+        settings.setTitle("MicroLogic");
+
+
+        application.setSettings(settings);
+        application.start();
+    }
+
+
 }
---
+++ src/main/java/ru/project/Main.java
@@ -0,0 +1,4 @@
+package ru.project;
+
+public class Main {
+}
